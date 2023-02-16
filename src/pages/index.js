fabcae3852490fd7bdbf0c5524bcce180cbd9b2e
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-02-16 20:48:04 +0800

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import Nav from './nav';
-import Main from './main-page'
+import Main from './main-page';
+import 'bootstrap/dist/css/bootstrap.css';
 export default function Home() {
   return (
     <html lang="zh-Hant-HK">
       <head>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
+  
         <meta charset="utf-8"/>
       </head>
       <body>
--- src/pages/nav.js
+++ src/pages/nav.js
@@ -1,13 +1,15 @@
 import Link from "next/link";
-
+import Image from "next/image";
 export default function Nav(){
     return (
         <div className="nav">
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <Link href="/"><Image src="logo.png"  width="300" height="200" className="d-inline-block align-top"/></Link>
                 <ul>
+                    <li></li>
                     <li><Link href="/" className="navbar-brand">Home</Link></li>
                     <li><Link href="/content" className="navbar-brand">電腦</Link></li>
-                </ul> 
+                </ul>
             </nav>
         </div>
     ); 
