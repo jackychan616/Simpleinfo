2d3b00586c349b5ce75f94a1b5523e81fedfbeb2
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-02-16 21:13:50 +0800

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,26 @@
 import Nav from './nav';
 import Main from './main-page';
 import 'bootstrap/dist/css/bootstrap.css';    
+import style from '../styles/Home.module.css'
+import Document, {
+  Html,
+  Head,
+  Main
+} from 'next/document'
+
+
+import { Children } from 'react'
+
 
 export default function Home() {
   return (
-    <html lang="zh-Hant-HK" >
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <Html lang="zh-Hant-HK" className={style.html}>
+      <Head>
         <meta charset="utf-8"/>
-      </head>
+      </Head>
       <body>
-        
-        <div >
-          <div>
-            <Nav/>
-          </div>
-          
-        </div>
+          <Main/>
       </body>
-    </html>
+    </Html>
   )
 }
