c4f3462c82f55a6750d88afdbdd29744d64631c1
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-02-16 19:10:47 +0800

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 
+
 export default function Home() {
   return (
     <html lang="zh-Hant-HK">
--- src/pages/nav.js
+++ src/pages/nav.js
@@ -3,7 +3,6 @@ import Link from "next/link";
 function Nav(){
     return (
         <div className="nav">
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
            <ul>
                 <li><Link href="/">Home</Link></li>
                 <li><Link href="/content">電腦</Link></li>
