f4277c0069df39eba9f29795219432a899f7738d
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-02-16 20:59:08 +0800

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
 import Nav from './nav';
 import Main from './main-page';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';    
+
 export default function Home() {
   return (
-    <html lang="zh-Hant-HK" className={styles.html}>
+    <html lang="zh-Hant-HK" >
       <head>
-  
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <meta charset="utf-8"/>
       </head>
       <body>
-        <div className={styles.description}>
+        <div >
           <Nav/>
         </div>
       </body>
--- src/pages/nav.js
+++ src/pages/nav.js
@@ -4,10 +4,8 @@ export default function Nav(){
     return (
         <div className="nav">
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <Link href="/"><Image src="logo.png"  width="300" height="200" className="d-inline-block align-top"/></Link>
+                <Link href="/"><Image src="/logo.png"  width="50" height="50" className="d-inline-block align-top"/></Link>
                 <ul>
-                    <li></li>
-                    <li><Link href="/" className="navbar-brand">Home</Link></li>
                     <li><Link href="/content" className="navbar-brand">電腦</Link></li>
                 </ul>
             </nav>
