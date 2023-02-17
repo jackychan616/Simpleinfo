87cc6db7bbc07e8391356426edd2cb7861148fa0
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-02-17 17:31:13 +0800

    bar 50%

--- src/pages/nav.js
+++ src/pages/nav.js
@@ -4,10 +4,34 @@ import Image from "next/image";
 export default function Nav(){
     return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <Link href="/"><Image src="/logo.png"  width="50" height="50" className="d-inline-block align-top"/></Link>
-                <ul>
-                    <li><Link href="/content" className="navbar-brand">電腦</Link></li>
-                </ul>
+                <Link href="/" className="navbar-brand">
+                    <Image src="/logo.png"  width="50" height="50" alt="" className="d-inline-block align-top"/>
+                    <span className="h3 mx-1">Simple Info</span>
+                </Link>
+
+                <div classNAme="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+
+                        <li className="nav-item active">
+                            <Link className="nav-link" href="/">首頁<span class="sr-only">(current)</span></Link>
+                        </li>
+
+                        <li class="nav-item">
+                            <Link className="nav-link" href="/">最近更新</Link>
+                        </li>
+                        
+                        <li class="nav-item">
+                            <Link className="nav-link" href="/content"> 教學文章</Link>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <Link className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> 更多</Link>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <Link className="dropdown-item" href="/">1</Link>
+                                    <Link className="dropdown-item" href="/">2</Link>
+                                </div>
+                        </li>
+                    </ul>
+                </div>
             </nav>
     ); 
 }
