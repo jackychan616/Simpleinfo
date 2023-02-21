d3aaa24ce234bba7a9a4b61810af3180f32933b2
Jason Chan <80382634+jasonchanjj123@users.noreply.github.com>
2023-02-21 06:43:00 +0000

    added container

--- src/pages/content/simple_linux_cmd.js
+++ src/pages/content/simple_linux_cmd.js
@@ -3,7 +3,7 @@ import Nav from '../components/nav'
 
 function Content(){
     return(
-        <div>
+        <div className="container">
             <div id="web-title">
             <h1>
                 Linux幾個必學指令(ls cd cat)
@@ -33,15 +33,14 @@ function Content(){
 
             
             <h3>其他指令</h3>
-            <code>
-            <pre>    ls '['路徑']' 顯示其他路徑文件
-    -d */     只顯示檔案
-    -R        顯示當前路徑中檔案子檔案
-    -l        顯示檔案資訊
-    -a        顯示隱藏檔案
-    -al       顯示隱藏檔案資訊
-            </pre>    
-            </code>
+            <code><pre> ls [路徑] 顯示其他路徑文件   </pre> </code>
+            <code><pre> -d */     只顯示檔案  </pre> </code>
+            <code><pre> -R        顯示當前路徑中檔案子檔案  </pre> </code>
+            <code><pre> -l        顯示檔案資訊  </pre> </code>
+            <code><pre> -a        顯示隱藏檔案  </pre> </code>
+            <code><pre> -al       顯示隱藏檔案資訊  </pre> </code>
+
+
             <Image src="/img/img1.png" id="img1" width='500' height="300"/>
         </div>
         <div class="content" id="content-cd">
@@ -67,20 +66,13 @@ function Content(){
             </h3>
             <p>cat 用途是獲取文件或輸入并打印出來,通常用來顯示文件內容處理文件</p>
             <h3>格式</h3>
-            <code>
-                <pre>
-    cat '['-字母']' '['文件名稱']'
-                </pre>
-            </code>
+            <code><pre> cat '['-字母']' '['文件名稱']'</pre></code>
             <h3>基本使用</h3>
-            <code>
-                <pre>
-    cat '['filename.txt']'     顯示檔案內容
-    cat '['文字或檔案名稱(file2name.txr)] '{'>'}' '['filename.txt']'   快速創建檔案並將內容寫入檔案
-    cat *.'['副檔名']'    例如 *.txt   顯示所有txt的檔案內容 
-                </pre>
-            </code>
-            
+            <code><pre> cat '['filename.txt']'     顯示檔案內容   </pre> </code>
+            <code><pre> cat '['文字或檔案名稱(file2name.txr)] '{'>'}' '['filename.txt']'   快速創建檔案並將內容寫入檔案   </pre> </code>
+            <code><pre> cat *.'['副檔名']'    例如 *.txt   顯示所有txt的檔案內容   </pre> </code>
+
+           
         </div>
         </div>
         
