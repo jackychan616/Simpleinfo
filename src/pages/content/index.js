867b956cda38efbc237edd1c3eecd808bddab1a6
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-02-22 20:13:51 +0800

    fix

--- src/pages/content/index.js
+++ src/pages/content/index.js
@@ -4,7 +4,7 @@ export default function Page(){
     return (
         <div>
            <Link href="/content/simple_linux_cmd">OK</Link>
-            
+            <Link href="/content/pork_21point_quick_talk"><p>123</p></Link>
         </div>
     );
 }
