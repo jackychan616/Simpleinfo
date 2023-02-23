9f6d579eca64e31fe4ac7b9a02f9931320f0ccb2
ubuntu <jasonchangames@GMAIL.COM>
2023-02-23 09:20:37 +0000

    added init page_layout.js

---
+++ src/pages/components/page_layout.js
@@ -0,0 +1,13 @@
+import Script from 'next/script';
+import Head from  'next/head';
+
+function Layout({children}){
+   return(
+       <div>
+	    
+       </div>
+    );
+
+}
+
+export default Layout;
