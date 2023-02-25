0ac812db0e2ee3bba775255049c7096f6eaa9a31
jackychan616 <82193805+jackychan616@users.noreply.github.com>
2023-02-25 10:39:10 +0800

    fix

--- src/pages/main-page.js
+++ src/pages/main-page.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
 function Body(){
     return(
-        <div className=" d-flex   justify-content-center align-items-center" style={{backgroundColor : "#37BC9B",height : "150px"}} >
-            <h1 className='display-5 ' >Simple info</h1>
-            <p> Keep it simple </p>
+        <div className=" d-flex   justify-content-center align-items-center" style={{backgroundColor : "rgb(56,60,68)",height : "150px"}}>
+            <h1 className='display-5 'style={{color : "white"}}>Simple info</h1>
+            <p style={{color : "white"}}> Keep it simple </p>
         </div>
     )
 }   
