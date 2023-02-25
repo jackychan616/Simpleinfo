f3d08c81a2c6a5c507ab3056c36e8ad4d75c99cb
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-02-25 09:56:58 +0800

    content add python

--- src/pages/content/index.js
+++ src/pages/content/index.js
@@ -2,11 +2,12 @@ import Link from "next/link";
 import Image from "next/image";
 
 
+
 export default function Page(){
     return (
         <div className="container">
             <div className="row">
-                <div className="col container-lg content-container">
+                <div className="col container-lg content-container border border-primary">
                     <Link className="content-link" href="/content/linux-tutorial"><h4>Linux 教學</h4></Link>
                     <Image src="/img/linux.jpeg" width="341" height="256"></Image>
 
@@ -15,7 +16,17 @@ export default function Page(){
                     <Link className="content-link" href="/content/card-game"><h4>桌遊</h4></Link>
                     <Image src="/img/poker.jpg" width="341" height="226"></Image>
                 </div>
+                
             </div>
+
+            <div className="row">
+                <div className="col container-lg content-container border border-primary">
+                    <Link href="/" className="content-link"><h4>Python 教學</h4></Link>
+                    <Image src="/img/python.png" width="314" height="150"></Image>
+                </div>  
+                <div className=""></div>
+            </div>
+            
                 
            
         </div>
