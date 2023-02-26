fd843a4655df2db12459bd1d01fecd978532ef4f
jackychan616 <82193805+jackychan616@users.noreply.github.com>
2023-02-26 17:39:54 +0800

    fix

--- src/pages/main-page.js
+++ src/pages/main-page.js
@@ -1,8 +1,53 @@
 import Link from "next/link";
 import Image from "next/image";
+import { createStyles, SimpleGrid, Card, Text, Container, AspectRatio } from '@mantine/core';
+
+const blogdata = [
+    {
+        title: 'Python 教學',
+        image: '',
+        date: 'September 12, 2022',
+    },
+    {
+        title: 'linux 教學',
+        image: '',
+        date: 'September 12, 2022',
+    }
+];
+const useStyles = createStyles((theme) => ({
+    card: {
+      transition: 'transform 150ms ease, box-shadow 150ms ease',
+  
+      '&:hover': {
+        transform: 'scale(1.01)',
+        boxShadow: theme.shadows.md,
+      },
+    },
+  
+    title: {
+      fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+      fontWeight: 600,
+    },
+  }));
+
+
 function Body(){
-    return(
-        <>
+            const { classes } = useStyles();
+            const cards = blogdata.map((article) => (
+            <Card key={article.title} p="md" radius="md" component="a" href="#" className={classes.card}>
+            <AspectRatio ratio={1920 / 1080}>
+                <Image src={article.image} />
+            </AspectRatio>
+            <Text color="dimmed" size="xs" transform="uppercase" weight={700} mt="md">
+                {article.date}
+            </Text>
+            <Text className={classes.title} mt={5}>
+                {article.title}
+            </Text>
+            </Card>
+            ));
+            return(
+                <>
             <div className=" d-flex   justify-content-center align-items-center" style={{backgroundColor : "rgb(56,60,68)",height : "150px"}}>
                 <h1 className='display-5 'style={{color : "white"}}>Simple info</h1>
                 <p style={{color : "white"}}> Keep it simple </p>
@@ -19,6 +64,11 @@ function Body(){
                     
                 </div>
             </div>
+            <Container py="xl">
+                <SimpleGrid cols={2} breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
+                {cards}
+                </SimpleGrid>
+            </Container>
         </>
         
     )
