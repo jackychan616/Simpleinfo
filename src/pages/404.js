88fb6a1b27f7bbc0e0b5751a245584ff04d4c7c3
jackychan616 <82193805+jackychan616@users.noreply.github.com>
2023-02-26 16:59:10 +0800

    fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,43 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { createStyles, Title, Text, Button, Container, Group } from '@mantine/core';
+const useStyles = createStyles((theme) => ({
+    root: {
+      paddingTop: 80,
+      paddingBottom: 120,
+      backgroundColor: theme.fn.variant({ variant: 'filled', color: theme.primaryColor }).background,
+    },
+  
+    label: {
+      textAlign: 'center',
+      fontWeight: 900,
+      fontSize: 220,
+      lineHeight: 1,
+      marginBottom: theme.spacing.xl * 1.5,
+      color: theme.colors[theme.primaryColor][3],
+  
+      [theme.fn.smallerThan('sm')]: {
+        fontSize: 120,
+      },
+    },
+}));
 function Notfound(){
+    const { classes } = useStyles();
     return(
-        <div className="container">
-            <Image src = "/img/404.png"  layout = "fill"  quality = {100}/>
-            <h2 >不好意思!這裡沒有任何東西</h2>
-            <Link href="/" style = {{textDecoration: 'none', color : 'white' ,display :"inline-block" ,backgroundColor : "Gray"}}>回到首頁</Link>
+        <>
+        <div className={classes.label}>404</div>
+        <div className="container" style = {{zIndex :-1 ,position : "fixed", width : "100vw" ,height : "100vh"}}></div>
+        <h2 style = {{textAlign: 'center'}}>不好意思!這裡沒有任何東西</h2>
+        <div>
+        <Group position = "center">
+            <Link href = "/">
+            <Button variant="subtle" size="md">
+                Back to the home page
+            </Button>
+            </Link>
+        </Group>
         </div>
+        </>
     );
 }
 
--- src/pages/components/nav.js
+++ src/pages/components/nav.js
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 export default function Nav(){
     return (
-            <nav className="  navbar navbar-expand-lg navbar-light bg-light " >
+            <nav className="  navbar navbar-expand-lg navbar-light bg-light">
                 <Link href="/" className="navbar-brand">
                     <Image src="/logo.png"  height='30' width='30' alt=""/>
                     <span className="h3 mx-1">Simple Info</span>
                 </Link>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent" >
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
 
                         <li className="nav-item active">
