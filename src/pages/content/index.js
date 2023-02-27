c5631e88f6e505fc3cedbe7705b1750fa27c3201
jackychan616 <82193805+jackychan616@users.noreply.github.com>
2023-02-27 20:27:04 +0800

    fix car postdata

--- src/pages/content/index.js
+++ src/pages/content/index.js
@@ -6,13 +6,13 @@ import ArtiCard from "../components/card";
 
 const PostData =[
     {
-        title: 'Python 教學',image: '/img/py.jpg',href: '/content/python-tutorial'
+        "title": 'Python 教學',"image": '/img/py.jpg',"href": '/content/python-tutorial',"date": 'September 12, 2022',
     },
     {
-        title: 'linux 教學',image: '/img/linux.jpeg',href: '/content/linux-tutorial' 
+        "title": 'linux 教學',"image": '/img/linux.jpeg',"href": '/content/linux-tutorial' ,"date": 'September 12, 2022',
     },
     {
-        title:"[porker 撲克牌] 二十一點玩法教學 ",image:"/img/poker.jpg",href:"/content/card-game"
+        "title":"[porker 撲克牌] 二十一點玩法教學 ","image":"/img/poker.jpg","href":"/content/card-game","date": 'September 12, 2022',
     },
     {
 
