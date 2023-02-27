1cb81ffc5b349d7d23852f183794ba82786e3796
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-02-27 16:56:27 +0800

    card

--- src/pages/components/card.js
+++ src/pages/components/card.js
@@ -1,112 +1,45 @@
-import { IconBookmark, IconHeart, IconShare } from '@tabler/icons';
-import {
-  Card,
-  Image,
-  Text,
-  ActionIcon,
-  Badge,
-  Group,
-  Center,
-  Avatar,
-  createStyles,
-} from '@mantine/core';
+import { createStyles, SimpleGrid, Card, Image, Text, Container, AspectRatio } from '@mantine/core';
+
+//PostData ={"title":str,"image":url,"date":str}
 
 const useStyles = createStyles((theme) => ({
   card: {
-    position: 'relative',
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
-  },
+    transition: 'transform 150ms ease, box-shadow 150ms ease',
 
-  rating: {
-    position: 'absolute',
-    top: theme.spacing.xs,
-    right: theme.spacing.xs + 2,
-    pointerEvents: 'none',
+    '&:hover': {
+      transform: 'scale(1.01)',
+      boxShadow: theme.shadows.md,
+    },
   },
 
   title: {
-    display: 'block',
-    marginTop: theme.spacing.md,
-    marginBottom: theme.spacing.xs / 2,
-  },
-
-  action: {
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-    ...theme.fn.hover({
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
-    }),
-  },
-
-  footer: {
-    marginTop: theme.spacing.md,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontWeight: 600,
   },
 }));
 
-interface ArticleCardProps {
-  image: string;
-  link: string;
-  title: string;
-  description: string;
-  rating: string;
-  author: {
-    name: string;
-    image: string;
-  };
-}
+export default function ArtiCard() {
+  const { classes } = useStyles();
 
-export function ArticleCard({
-  className,
-  image,
-  link,
-  title,
-  description,
-  author,
-  rating,
-  ...others
-}: ArticleCardProps & Omit<React.ComponentPropsWithoutRef<'div'>, keyof ArticleCardProps>) {
-  const { classes, cx, theme } = useStyles();
-  const linkProps = { href: link, target: '_blank', rel: 'noopener noreferrer' };
+  const cards = PostData.map((article) => (
+    <Card key={article.title} p="md" radius="md" component="a" href="#" className={classes.card}>
+      <AspectRatio ratio={1920 / 1080}>
+        <Image src={article.image} />
+      </AspectRatio>
+      <Text color="dimmed" size="xs" transform="uppercase" weight={700} mt="md">
+        {article.date}
+      </Text>
+      <Text className={classes.title} mt={5}>
+        {article.title}
+      </Text>
+    </Card>
+  ));
 
   return (
-    <Card withBorder radius="md" className={cx(classes.card, className)} {...others}>
-      <Card.Section>
-        <a {...linkProps}>
-          <Image src={image} height={180} />
-        </a>
-      </Card.Section>
-
-      <Badge className={classes.rating} variant="gradient" gradient={{ from: 'yellow', to: 'red' }}>
-        {rating}
-      </Badge>
-
-      <Text className={classes.title} weight={500} component="a" {...linkProps}>
-        {title}
-      </Text>
-
-      <Text size="sm" color="dimmed" lineClamp={4}>
-        {description}
-      </Text>
-
-      <Group position="apart" className={classes.footer}>
-        <Center>
-          <Avatar src={author.image} size={24} radius="xl" mr="xs" />
-          <Text size="sm" inline>
-            {author.name}
-          </Text>
-        </Center>
-
-        <Group spacing={8} mr={0}>
-          <ActionIcon className={classes.action}>
-            <IconHeart size={16} color={theme.colors.red[6]} />
-          </ActionIcon>
-          <ActionIcon className={classes.action}>
-            <IconBookmark size={16} color={theme.colors.yellow[7]} />
-          </ActionIcon>
-          <ActionIcon className={classes.action}>
-            <IconShare size={16} />
-          </ActionIcon>
-        </Group>
-      </Group>
-    </Card>
+    <Container py="xl">
+      <SimpleGrid cols={2} breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
+        {cards}
+      </SimpleGrid>
+    </Container>
   );
 }
