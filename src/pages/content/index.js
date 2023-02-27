1fc72e93487c603364ca3fc7ff36ccf13214ff6b
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-02-27 18:09:31 +0800

    fix

--- src/pages/content/index.js
+++ src/pages/content/index.js
@@ -1,34 +1,30 @@
 import Link from "next/link";
 import Image from "next/image";
+import { Container } from "react-bootstrap";
+import ArtiCard from "@/pages/components/card";
+
+const PostData =[
+    {
+        title: 'Python 教學',image: '/img/py.jpg',"date": 'September 12, 2022',href: '/content/python-tutorial'
+    },
+    {
+        title: 'linux 教學',image: '/img/linux.jpeg',date: 'September 12, 2022',href: '/content/linux-tutorial' 
+    },
+    {
+        title:"[porker 撲克牌] 二十一點玩法教學 ",image:"/img/porker.jpg",date:"September 12, 2022",href:"/content/card-game"
+    },
+    {
+        
+    }
 
 
+]
 
 export default function Page(){
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col container-lg content-container border border-primary">
-                    <Link className="content-link" href="/content/linux-tutorial"><h4>Linux 教學</h4></Link>
-                    <Image src="/img/linux.jpeg" width="341" height="256"></Image>
-
-                </div>
-            </div>
-            <div className="row">
-            <div className="col container-lg content-container border border-primary">
-                    <Link className="content-link" href="/content/card-game/poker_21point_quick_talk"><h4>桌遊</h4></Link>
-                    <Image src="/img/poker.jpg" width="341" height="226"></Image>
-            </div>
-            </div>
-
-            <div className="row">
-                <div className="col container-lg content-container border border-primary">
-                    <Link href="/content/python-tutorial" className="content-link"><h4>Python 教學</h4></Link>
-                    <Image src="/img/python.png" width="314" height="150"></Image>
-                </div>  
-            </div>
-            
-                
-           
-        </div>
+        <Container>
+            <ArtiCard PostData={PostData}/>
+        </Container>
+       
     );
 }
