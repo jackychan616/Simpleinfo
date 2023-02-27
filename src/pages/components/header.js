46f2796925d9a9b8f526b2dba2ec733f217d8143
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-02-27 18:48:19 +0800

    fix

--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -31,24 +31,7 @@ function Header(props) {
           Sign up
         </Button>
       </Toolbar>
-      <Toolbar
-        component="nav"
-        variant="dense"
-        sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
-      >
-        {sections.map((section) => (
-          <Link
-            color="inherit"
-            noWrap
-            key={section.title}
-            variant="body2"
-            href={section.url}
-            sx={{ p: 1, flexShrink: 0 }}
-          >
-            {section.title}
-          </Link>
-        ))}
-      </Toolbar>
+      
     </React.Fragment>
   );
 }
