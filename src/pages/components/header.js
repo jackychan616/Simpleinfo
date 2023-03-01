bbcabc05cb6b2021cdb0b06beaaed6840166b9d5
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-03-01 20:17:31 +0800

    header

--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -1,46 +1,189 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import Image from 'next/image';
-function Header(props) {
-  const {title } = props;
+import {
+  createStyles,
+  Header,
+  HoverCard,
+  Group,
+  Button,
+  UnstyledButton,
+  Text,
+  SimpleGrid,
+  ThemeIcon,
+  Anchor,
+  Divider,
+  Center,
+  Box,
+  Burger,
+  Drawer,
+  Collapse,
+  ScrollArea,
+  Title
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { blue } from '@mui/material/colors';
+import Link from 'next/link';
+
+
+const useStyles = createStyles((theme) => ({
+  link: {
+    display: 'flex',
+    alignItems: 'center',
+    height: '100%',
+    paddingLeft: theme.spacing.md,
+    paddingRight: theme.spacing.md,
+    textDecoration: 'none',
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    fontWeight: 600,
+    fontSize: theme.fontSizes.sm,
+
+    [theme.fn.smallerThan('sm')]: {
+      height: 42,
+      display: 'flex',
+      alignItems: 'center',
+      width: '100%',
+    },
+
+    ...theme.fn.hover({
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+    }),
+  },
+
+  subLink: {
+    width: '100%',
+    padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
+    borderRadius: theme.radius.md,
+
+    ...theme.fn.hover({
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[0],
+    }),
+
+    '&:active': theme.activeStyles,
+  },
+
+  dropdownFooter: {
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[0],
+    margin: -theme.spacing.md,
+    marginTop: theme.spacing.sm,
+    padding: `${theme.spacing.md}px ${theme.spacing.md * 2}px`,
+    paddingBottom: theme.spacing.xl,
+    borderTop: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1]
+    }`,
+  },
+
+  hiddenMobile: {
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  hiddenDesktop: {
+    [theme.fn.largerThan('sm')]: {
+      display: 'none',
+    },
+  },
+}));
+
+
+
+export default function HeaderMegaMenu() {
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
+  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
+  const { classes, theme } = useStyles();
+
+
 
   return (
-    <React.Fragment>
-      <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' ,bgcolor : "#b3d9ff"}}>
-        <Link href="/" className="navbar-brand">
-                    <Image src="/logo.png"  height='30' width='30' alt=""/>
-                    <span className="h3 mx-1">Simple Info</span>
-        </Link>
-        <Typography
-          component="h2"
-          variant="h5"
-          color="white"
-          align="center"
-          noWrap
-          sx={{ flex: 1 }}
-        >
-          {title}
-        </Typography>
-        <IconButton>
-          <SearchIcon />
-        </IconButton>
-        <Button variant="outlined" size="small" color = "inherit">
-          Sign up
-        </Button>
-      </Toolbar>
+    <Box pb={20}  >
+      <Header height={60} px="md" >
+        <Group position="apart" sx={{ height: '100%' }}>
+          
+          <Group sx={{ height: '100%' }} spacing={0} className={classes.hiddenMobile}>
+            <Title>Simple Info</Title>
+            <Link href="/" className={classes.link}>
+              <Box component="span" mr={5}>
+                      首頁
+              </Box>
+            </Link>
+            <HoverCard width={600} position="bottom" radius="md" shadow="md" withinPortal>
+              <HoverCard.Target>
+                <Link href="/content" className={classes.link}>
+                  <Center inline>
+                    <Box component="span" mr={5}>
+                      文章
+                    </Box>
+
+                  </Center>
+                </Link>
+              </HoverCard.Target>
+
+              <HoverCard.Dropdown sx={{ overflow: 'hidden' }}>
+                <Group position="apart" px="md">
+                  <Text weight={500}>Features</Text>
+                  <Anchor href="/content" size="xs">
+                    View all
+                  </Anchor>
+                </Group>
+
+                <Divider
+                  my="sm"
+                  mx="-md"
+                  color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'}
+                />
+                
+
+                <div className={classes.dropdownFooter}>
+                  <Group position="apart">
+                    <div>
+                      <Text weight={500} size="sm">
+                        Get started
+                      </Text>
+                      <Text size="xs" color="dimmed">
+                        Their food sources have decreased, and their numbers
+                      </Text>
+                    </div>
+                    <Button variant="default">Get started</Button>
+                  </Group>
+                </div>
+              </HoverCard.Dropdown>
+            </HoverCard>
+            
+          </Group>
+
+          <Group className={classes.hiddenMobile}>
+            <Button>訂閱</Button>
+          </Group>
+
+          <Burger opened={drawerOpened} onClick={toggleDrawer} className={classes.hiddenDesktop} />
+        </Group>
+      </Header>
+
       
-    </React.Fragment>
+      <Drawer
+        opened={drawerOpened}
+        onClose={closeDrawer}
+        size="100%"
+        padding="md"
+        title="Navigation"
+        className={classes.hiddenDesktop}
+        zIndex={1000000}
+      >
+        <ScrollArea sx={{ height: 'calc(100vh - 60px)' }} mx="-md">
+          <Divider my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'} />
+
+          <Link href="/" className={classes.link}>
+            首頁
+          </Link>
+          <UnstyledButton className={classes.link} onClick={toggleLinks}>
+            <Center inline>
+              <Box component="span" mr={5}>
+                文章
+              </Box>
+            </Center>
+          </UnstyledButton>
+          
+
+        </ScrollArea>
+      </Drawer>
+    </Box>
   );
 }
-
-Header.propTypes = {
-  title: PropTypes.string.isRequired,
-};
-
-export default Header;
