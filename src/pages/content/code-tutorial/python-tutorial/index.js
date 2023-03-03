e435d89adc8f5d0ac84a376f7a9013b2a4819c7f
jackychan616 <82193805+jackychan616@users.noreply.github.com>
2023-03-03 11:38:55 +0800

    fix

--- src/pages/content/code-tutorial/python-tutorial/index.js
+++ src/pages/content/code-tutorial/python-tutorial/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function page(){
     return(
         <>
-            <Link href="/content/python-tutorial/python-quick-tutorial">Python 快速入門</Link>
+            <Link href="/content/code-tutorial/python-tutorial/python-quick-tutorial">Python 快速入門</Link>
         </>
     );
 }
