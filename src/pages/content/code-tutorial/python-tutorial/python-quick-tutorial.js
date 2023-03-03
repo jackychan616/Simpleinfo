8bdfeca1123afcb5afd9862bdbc8a01489def0a2
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-03-03 13:41:09 +0800

    fix

--- src/pages/content/code-tutorial/python-tutorial/python-quick-tutorial.js
+++ src/pages/content/code-tutorial/python-tutorial/python-quick-tutorial.js
@@ -1,24 +1,22 @@
 import Head from "next/head";
-import {Title,Image,Text,Table,Code} from "@mantine/core";
-
-
-import {ConText,OutputText,PyPrism} from "../../../components/component";
+import {TitleImage,Text,Table,Space,Image} from "@mantine/core";
+import {ConText,OutputText,PyPrism,ConTitle} from "../../../components/component";
 import Sharebox from "../../../components/share";
 
-const code1=`
+const kbd1=`
 print('ok')
 `;
 
-const code2=`
+const kbd2=`
 number = 1 
 print(number) 
 `;
 
-const code3=`
+const kbd3=`
 text = '這是一行句子' 
 print(text) 
 `;
-const code4=`
+const kbd4=`
 Bool =True
 if Bool :
     //Bool 是True則執行這一行
@@ -26,7 +24,7 @@ else :
     //Bool 是False則執行這一行
 `;
 
-const code5=`
+const kbd5=`
 age = 16
 if age >=18:
     print('adult')
@@ -34,10 +32,10 @@ else:
     print('kids')
 `;
 const elements=[
-    {name:'str',type:'句子',example:`'s=這是一行句子'`,code:`print(s+',這也是')`,output:`'這是一行句子,這也是`},
-    {name:'int',type:'整數(1 , 2 ,3)',example:'number= 120',code:`print(number+50)`,output:'170'},
-    {name:"list",type:`列表,列表中可以包含多種變數,每個元素都有分配一個索引，第一個索引是0，第二個索引是1，如此類推`,example:`list=['apple','orange',1,2,3]`,code:`print(list[0]+list[0])`,output:`'appleorange`},
-    {name:'dict',type:'字典,能夠儲存變數,每個數據都有獨特的名稱作為索引',example:`dict={'number1':1,'number2':2}`,code:`print(dict['number1']+dict['number2'])`,output:'3'}];
+    {name:'str',type:'句子',example:`s='這是一行句子'`,kbd:`print(s+',這也是')`,output:`'這是一行句子,這也是`},
+    {name:'int',type:'整數(1 , 2 ,3)',example:'number= 120',kbd:`print(number+50)`,output:'170'},
+    {name:"list",type:`列表,列表中可以包含多種變數,每個元素都有分配一個索引，第一個索引是0，第二個索引是1，如此類推`,example:`list=['apple','orange',1,2,3]`,kbd:`print(list[0]+list[0])`,output:`'appleorange`},
+    {name:'dict',type:'字典,能夠儲存變數,每個數據都有獨特的名稱作為索引',example:`dict={'number1':1,'number2':2}`,kbd:`print(dict['number1']+dict['number2'])`,output:'3'}];
 
 
     function Page(){
@@ -46,7 +44,7 @@ const elements=[
           <td>{element.name}</td>
           <td>{element.type}</td>
           <td>{element.example}</td>
-          <td>{element.code}</td>
+          <td>{element.kbd}</td>
           <td>{element.output}</td>
         </tr>
       ));
@@ -59,11 +57,11 @@ const elements=[
             <meta name="description" content="入門快速學習Python,即使新手也很快速掌握"></meta>
         </Head>
         <div className="container">
-            <Title order={1}>Python入門</Title>
+            <ConTitle order={1}>Python入門</ConTitle>
 
             <div >
                 <div className="page_container">
-                    <Title order={2}>簡介</Title>
+                    <ConTitle order={2}>簡介</ConTitle>
       
                     <ConText >Python 是一種易學、強大的程式語言，
                     Python的設計哲學強調程式碼的可讀性和簡潔的語法，
@@ -73,14 +71,14 @@ const elements=[
                 </div>
 
                 <div className="page_container">
-                    <Title order={2}>下載Python</Title>
+                    <ConTitle order={2}>下載Python</ConTitle>
                     <ConText fz="lg">我們將介紹如何在電腦設置Python開發環境</ConText>
                     <ConText fz="lg">Python能在大多數平臺使用包括Windows,MacOS,Linux 等等</ConText>
-                    <Title order={3}>下載</Title>
+                    <ConTitle order={3}>下載</ConTitle>
                     <ConText fz="lg">一般建議在Python官網下載 <a href="https://www.python.org/downloads/">https://www.python.org/downloads/</a></ConText>
 
-                    <Title order={3}>安裝</Title>
-                    <ConText fz="lg">下載了合適的檔案後，按 <kbd>win + r </kbd>然後輸入<kbd>cmd</kbd> 打開終端，然後輸入python檢查是否安裝正確</ConText>
+                    <ConTitle order={3}>安裝</ConTitle>
+                    <ConText fz="lg">下載了合適的檔案後，按 <kbd>win + r </kbd>然後輸入<kbd>cmd</kbd > 打開終端，然後輸入python檢查是否安裝正確</ConText>
                     <Image src="/img/python-install-check.png" alt="" ></Image>
                     <ConText>如果輸出與圖片差不多，證明下載正確</ConText>
                     <Image src="/img/python-tutorial-cmd.png" alt="" ></Image>
@@ -88,35 +86,35 @@ const elements=[
                 </div>
 
                 <div className="page_container container">
-                    <Title order={3}>語法</Title>
+                    <ConTitle order={3}>語法</ConTitle>
                     <ConText>文件輸入以下句子</ConText>
-                    <PyPrism language="python">{code1}</PyPrism>
+                    <PyPrism language="python">{kbd1}</PyPrism>
                     <ConText>輸出</ConText>
                     <samp>ok</samp>
-                    <Title order={4}>Python 標識符</Title>
+                    <ConTitle order={4}>Python 標識符</ConTitle>
                     <Text>在Python中, 標識符由英文字母、符號及橫綫{`(_)`}組成</Text>
                     <Text>但不能由數字做開頭</Text>
-                    <Text>能夠以大小寫區分,例如<Code>Print</Code>和<Code>print</Code>不一樣</Text>
-                    
+                    <ConText>能夠以大小寫區分,例如<kbd>Print </kbd>和<kbd>print</kbd>不一樣</ConText>
+                    <ConTitle order={4}>標識符例子</ConTitle>
                 </div>
                 
                 <div className="container">
-                    <Title order={3}>變數</Title>
+                    <ConTitle order={3}>變數</ConTitle>
                     <ConText>在設計程式當中，我們常常需要儲存數據，我們能使用<kbd>變數</kbd>儲存數字和字串</ConText>
                     <ul>
                         <li><ConText fz="lg">每個變數被使用前必須賦值</ConText></li>
-                        <li><ConText><kbd>=</kbd>用來賦值給變數</ConText></li>
+                        <li><ConText><kbd>=</kbd >用來賦值給變數</ConText></li>
                         <li><ConText>每個變數被使用前必須賦值</ConText></li>
                         <li><ConText><kbd>變數 = 值</kbd></ConText></li>
                     </ul>
                             
 
-                        <PyPrism >{code2}</PyPrism>
+                        <PyPrism >{kbd2}</PyPrism>
                         <OutputText >結果:1</OutputText>
-                        <PyPrism >{code3}</PyPrism>
+                        <PyPrism >{kbd3}</PyPrism>
                 </div>
         <div className="container">
-            <Title order={4}>變數類型</Title>
+            <ConTitle order={4}>變數類型</ConTitle>
             <ConText>Python 有很多變數類型,用來處理數字和文字</ConText>
             <Table horizontalSpacing="xs" striped highlightOnHover withBorder withColumnBorders >
                 <thread>
@@ -133,13 +131,13 @@ const elements=[
         </div>    
         
         <div>
-            <Title order={2}>條件句</Title>
+            <ConTitle order={2}>條件句</ConTitle>
             <ConText>條件句是基於布林(Boolean 即是True & False)執行不同段落</ConText>
-            <Title order={3}>例子 1 </Title>
-            <PyPrism >{code4}</PyPrism>
-            <Title order={4}>例子 2</Title>
+            <ConTitle order={3}>例子 1 </ConTitle>
+            <PyPrism >{kbd4}</PyPrism>
+            <ConTitle order={4}>例子 2</ConTitle>
             <Text>結合運算符</Text>
-            <PyPrism >{code5}</PyPrism>
+            <PyPrism >{kbd5}</PyPrism>
         </div>
         
             </div>  
@@ -158,7 +156,6 @@ export default function MainPage(){
             <div>
                 <Sharebox/>
             </div>
-            
             <Page />
         </>
     );
