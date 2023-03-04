9f231a66108ac0deaf53165e15d5431ed8444dd1
jackychan616 <82193805+jackychan616@users.noreply.github.com>
2023-03-04 13:54:00 +0800

    add sub at down

--- src/pages/components/layout.js
+++ src/pages/components/layout.js
@@ -1,5 +1,7 @@
 import Header from './header';
-import { Text, Paper, Group ,Image} from '@mantine/core';
+import { Text, Paper, Group ,Image, Box, TextInput} from '@mantine/core';
+
+
 function Down(){
 
     return(
@@ -7,6 +9,9 @@ function Down(){
     <Paper withBorder p="lg" radius="md" shadow="md" style = {{backgroundColor : "rgba(82,113,255,255)"}}>
         <Group position="apart" mb="xs">
             <Image src ="/img/simple_info.png" alt = "icon" width = "120px" height="150px"/>
+            <Box position= "right" width = "500" height = "500">
+                <TextInput label = "Your gmail" placeholder = "電郵" style = {{color : "white"}}/>
+            </Box>
         </Group>
         <Text color="White"  fw ={1500}>
             built by simple info team
