1273e09cff85a480d26cc8fed708f9401f0e75dd
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-03-04 23:30:30 +0800

    fix

--- src/pages/content/code-tutorial/linux-tutorial/simple_linux_cmd.js
+++ src/pages/content/code-tutorial/linux-tutorial/simple_linux_cmd.js
@@ -1,20 +1,20 @@
 import Image from "next/image";
-
+import {ConTitle} from '../../../components/component';
 
 function ConContent(){
     return(
         <>
         <div className="container page_container">
             <div id="web-title">
-            <h1>
+            <ConTitle order={1}>
                 Linux幾個必學指令(ls cd cat)
-            </h1>
+            </ConTitle>
             </div>
 
         <div class="content" id="introduction">
-            <h2 id="introduction-title">
+            <ConTitle id="introduction-title">
                 為甚麼用Linux?
-            </h2>
+            </ConTitle>
             
             <p id="introduction-cotent">
                 Linux 是一種開放和免費的源代碼，可以替代window 作系統用。另外，Linux 的操作系統提供了不同工具和支持多種語言令開發者更自由選擇。
@@ -23,17 +23,17 @@ function ConContent(){
         </div>
         
         <div class="content" id="content-ls">
-            <h2 id="point1">
+            <ConTitle order={2}>
                 ls
-            </h2>
+            </ConTitle>
             
-            <h3>
+            <ConTitle order={3}>
                 用途
-            </h3>
+            </ConTitle>
             <p>顯示當前路徑文件</p>
 
             
-            <h3>其他指令</h3>
+            <ConTitle>其他指令</ConTitle>
             <code><pre> ls [路徑] 顯示其他路徑文件   </pre> </code>
             <code><pre> -d */     只顯示檔案  </pre> </code>
             <code><pre> -R        顯示當前路徑中檔案子檔案  </pre> </code>
@@ -45,30 +45,30 @@ function ConContent(){
             <Image src="/img/img1.png" id="img1" width='500' height="300" alt = " linux"/ >
         </div>
         <div class="content" id="content-cd">
-            <h1>
+            <ConTitle order={3}>
                 cd
-            </h1>
-            <h3>
+            </ConTitle>
+            <ConTitle order={3}>
                 用途
-            </h3>
+            </ConTitle>
             <p>切換路徑</p>
-            <h3>
+            <ConTitle order={3}>
                 示範
-            </h3>
+            </ConTitle>
             <Image src="/img/img2.png" id="img2" width="500" height='100' alt = " linux"/>
         </div>
 
         <div class="content" id="content-cat">
-            <h1>
+            <ConTitle>
                 cat
-            </h1>
-            <h3>
+            </ConTitle>
+            <ConTitle>
                 用途
-            </h3>
+            </ConTitle>
             <p>cat 用途是獲取文件或輸入并打印出來,通常用來顯示文件內容處理文件</p>
-            <h3>格式</h3>
+            <ConTitle>格式</ConTitle>
             <code>{` cat [-字母] [文件名稱]`}</code>
-            <h3>基本使用</h3>
+            <ConTitle>基本使用</ConTitle>
             <code>{`cat filename.txt]     顯示檔案內容  `}</code>
             <code>{` cat ['文字或檔案名稱(file2name.txr)] {'>'} ['filename.txt']   快速創建檔案並將內容寫入檔案 `}</code>
             <code>{`cat *.['副檔名']    例如 *.txt   顯示所有txt的檔案內容 `} </code>          
