1385adc4a1afd2b9b83cde462d9b0035852942c7
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-03-04 18:32:29 +0800

    fix

--- src/pages/content/index.js
+++ src/pages/content/index.js
@@ -6,10 +6,10 @@ import {ArtiCard} from "../components/card";
 
 const PostData =[
     {
-        name: '電腦編程教學',img: '/img/py.jpg',path: '/content/python-tutorial',date:''
+        name: '電腦編程教學',img: '/img/py.jpg',path: '/content/code-tutorial/python-tutorial',date:''
     },
     {
-        name: 'linux 教學',img: '/img/linux.jpeg',path: '/content/linux-tutorial' ,date:''
+        name: 'linux 教學',img: '/img/linux.jpeg',path: '/content/code-tutorial/linux-tutorial' ,date:''
     },
     {
         name:"[porker 撲克牌] 二十一點玩法教學 ",img:"/img/poker.jpg",path:"/content/card-game",date:''
