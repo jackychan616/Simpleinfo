c111c961cf518fb6203ec62eb9c9dd54270ea325
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-03-04 18:16:27 +0800

    adsense

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,8 @@ import Layout  from './components/layout';
 export default function MyApp({ Component ,pageProps}) {
   return  (
     <Layout>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3459129696587268"
+     crossorigin="anonymous"></script>
       <Component {...pageProps} />
     </Layout>
     
