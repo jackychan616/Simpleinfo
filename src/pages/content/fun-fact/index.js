0fdc06ebad574a81271a424a90f9e0fc613206f2
jackychan616 <82193805+jackychan616@users.noreply.github.com>
2023-03-06 19:37:34 +0800

    add sun yat sen fix index of content bug

--- src/pages/content/fun-fact/index.js
+++ src/pages/content/fun-fact/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head';
 import { Container } from '@mantine/core';
 import { ArtiCard } from '../../components/card';
+import Meta from '../../components/meta';
 
 const BlogData=[
-    {name:'中國古代最重要的君主(上)',path:'chinese-king1',img:'/img/chinese-king1.jpg',date:''},
-    {name:'中國古代最重要的君主(下)',path:'chinese-king2',img:'/img/chinese-king2.jpeg',date:''}
+    {name:'中國古代最重要的君主(上)',path:'chinese-king1',img:'/img/chinese-king1.jpg',date:'3/3/2023'},
+    {name:'中國古代最重要的君主(下)',path:'chinese-king2',img:'/img/chinese-king2.jpeg',date:'3/3/2023'},
+    {name:'古人的性癖好 孫中山是蘿莉控?',path:'Sun-Yat-sen',img:'/img/Sun-Yat-sen.jpeg',date:'6/3/2023'}
 ]
 
 function ConPage(){
@@ -21,7 +23,8 @@ export default function Page (){
             <Head>
                 <title>有趣小知識</title>
             </Head>
-            <ConPage/>
+            <Meta description="有趣小知識,中國近代史,中國歷史"/>
+            <ArtiCard data={BlogData}/>
         </>
     );
 }
