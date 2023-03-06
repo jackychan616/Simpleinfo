c85496e3896ee5f17365f89db8974703f30db494
jackychan616 <82193805+jackychan616@users.noreply.github.com>
2023-03-06 21:33:35 +0800

    fix Sun Yat Sen img

--- src/pages/content/fun-fact/Sun-Yat-sen.js
+++ src/pages/content/fun-fact/Sun-Yat-sen.js
@@ -18,11 +18,11 @@ function Body(){
             <ConText>
                 孫中山有一位伴侶（非正妻），叫大月薰。據說孫中山在流亡日本時認識了友人的女兒，那人即大月薰（當時10歲）。過後孫文愛上了她並向其父大月素堂提親，在一番波折後大月薰在15歲時與孫文結婚。
             </ConText>
-            <Image src="/img/Sun Yat-sen-1.jpg" height={"600px"} width = {"300px"}></Image>       
+            <Image src="/img/Sun Yat-sen-2.jpg"></Image>
             <ConText>
             另外，孫中山的二妻宋慶齡，比孫中山年輕27歲，即使在宋家的反對之下，孫文亦堅持與之結婚。
             </ConText>
-            <Image src="/img/Sun Yat-sen-2.jpg"></Image>
+            <Image src="/img/Sun Yat-sen-1.jpg" height={"600px"} width = {"300px"}></Image>       
         </Container>
     );
 }
