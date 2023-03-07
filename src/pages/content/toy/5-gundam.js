8d38288ebe48a4eaa99d04c1d363bf9497a7be31
Jason Chan <80382634+jasonchanjj123@users.noreply.github.com>
2023-03-07 01:09:14 +0000

    30%

--- src/pages/content/toy/5-gundam.js
+++ src/pages/content/toy/5-gundam.js
@@ -10,8 +10,12 @@ function ConPage(){
             <ConText >Hi-Nu高達是小說《機動戰士高達馬沙之反擊, 貝托蒂嘉的子嗣》中主角阿寶的MS，精密細緻的組件，配搭清晰分明的分色，並且擁有一流的靈活度，不失為一件良好的RG商品。</ConText>
             <Space h="lg"/>
             <Image src="/img/5-gundam/1.5.jfif" alt="Hi-Nu"/>
+            <Space h="sm"/>
+            <ConText>推薦度：⭐️⭐️⭐️⭐️⭐️</ConText>
             <Space h="lg"/>
-            
+            <ConTitle order={2}>2.RG 1/144 RX-93 ν GUNDAM</ConTitle>
+            <ConText>《機動戰士高達：馬沙之反擊》中主角阿寶的MS，全身由頭至腳的靈活度，組件亦不容易掉下，配上穩固的站立，是件必收貨品之一。需注意背後的浮游炮重量可能使機體不穩，傾斜等情況。</ConText>
+
         </Container>
             
    );
