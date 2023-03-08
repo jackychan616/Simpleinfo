74371cb34939d9792c4f7dd61e37b4a3cacc5164
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-03-08 21:57:36 +0800

    fix

--- src/pages/content/code-tutorial/linux-tutorial/simple_linux_cmd.js
+++ src/pages/content/code-tutorial/linux-tutorial/simple_linux_cmd.js
@@ -1,18 +1,18 @@
-import Image from "next/image";
+
 import {ConTitle} from '../../../components/component';
-import {Container} from '@mantine/core';
+import {Container,Image} from '@mantine/core';
 function ConContent(){
     return(
         <>
-        <Container >
-            <Container id="web-title">
+        <Container>
+
             <ConTitle order={1}>
                 Linux幾個必學指令(ls cd cat)
             </ConTitle>
-            </Container>
 
-        <Container >
-            <ConTitle id="introduction-title">
+
+        
+            <ConTitle order={2}id="introduction-title">
                 為甚麼用Linux?
             </ConTitle>
             
@@ -20,9 +20,9 @@ function ConContent(){
                 Linux 是一種開放和免費的源代碼，可以替代window 作系統用。另外，Linux 的操作系統提供了不同工具和支持多種語言令開發者更自由選擇。
             </p>
 
-        </Container>
+
         
-        <Container >
+        
             <ConTitle order={2}>
                 ls
             </ConTitle>
@@ -43,8 +43,7 @@ function ConContent(){
 
 
             <Image src="/img/img1.png" id="img1" width='500' height="300" alt = " linux"/ >
-        </Container>
-        <Container class="content" id="content-cd">
+        
             <ConTitle order={3}>
                 cd
             </ConTitle>
@@ -56,24 +55,23 @@ function ConContent(){
                 示範
             </ConTitle>
             <Image src="/img/img2.png" id="img2" width="500" height='100' alt = " linux"/>
-        </Container>
+        
 
-        <Container >
-            <ConTitle>
+        
+            <ConTitle order={2}>
                 cat
             </ConTitle>
-            <ConTitle>
+            <ConTitle order={3}>
                 用途
             </ConTitle>
             <p>cat 用途是獲取文件或輸入并打印出來,通常用來顯示文件內容處理文件</p>
-            <ConTitle>格式</ConTitle>
+            <ConTitle order={3}>格式</ConTitle>
             <code>{` cat [-字母] [文件名稱]`}</code>
-            <ConTitle>基本使用</ConTitle>
+            <ConTitle order={3}>基本使用</ConTitle>
             <code>{`cat filename.txt]     顯示檔案內容  `}</code>
             <code>{` cat ['文字或檔案名稱(file2name.txr)] {'>'} ['filename.txt']   快速創建檔案並將內容寫入檔案 `}</code>
             <code>{`cat *.['副檔名']    例如 *.txt   顯示所有txt的檔案內容 `} </code>          
-        </Container>
-        </Container>
+       </Container>
     </>
     );
 }
