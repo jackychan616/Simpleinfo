ea2f633b845fd14ab4999d623f08abd633a017a4
Jason Chan <80382634+jasonchanjj123@users.noreply.github.com>
2023-03-08 10:24:02 +0800

    fix

--- src/pages/content/sport/index.js
+++ src/pages/content/sport/index.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Container } from "@mantine/core";
-import {ArtiCard} from "../components/card";
-import Meta from "../components/meta";
+import {ArtiCard} from "../../components/card";
+import Meta from "../../components/meta";
 import { Page } from '../../components/page_index';
 const BlogList=[
     {name:"海布里之王-亨利",path:"sport/football-henry",date:'',img:'https://library.sportingnews.com/styles/facebook_1200x630/s3/2021-08/world-cup-trophy-stand_133kk6nwawbmj16uhxvaiznup2.jpg?itok=bYPSCbur'}
