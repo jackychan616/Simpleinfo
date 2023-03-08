d36cd71bb9949d7c062dd1b3203700f91debf5e7
Jason Chan <80382634+jasonchanjj123@users.noreply.github.com>
2023-03-08 10:16:58 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 
 import App from './main-page';
-import 'bootstrap/dist/css/bootstrap.css';    
-import style from '../styles/Home.module.css';
+
+
 import { Children } from 'react';
 import Head from 'next/head';
 export default function Home() {
