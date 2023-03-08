4d9916670d9f3b4d6992cae345cbfd08c045e90f
Jason Chan <80382634+jasonchanjj123@users.noreply.github.com>
2023-03-08 01:20:03 +0000

    fix

--- src/pages/content/code-tutorial/linux-tutorial/simple_linux_cmd.js
+++ src/pages/content/code-tutorial/linux-tutorial/simple_linux_cmd.js
@@ -1,17 +1,18 @@
 import Image from "next/image";
+import { Container } from "tabler-icons-react";
 import {ConTitle} from '../../../components/component';
 
 function ConContent(){
     return(
         <>
-        <div className="container page_container">
-            <div id="web-title">
+        <Container >
+            <Container id="web-title">
             <ConTitle order={1}>
                 Linux幾個必學指令(ls cd cat)
             </ConTitle>
-            </div>
+            </Container>
 
-        <div class="content" id="introduction">
+        <Container >
             <ConTitle id="introduction-title">
                 為甚麼用Linux?
             </ConTitle>
@@ -20,9 +21,9 @@ function ConContent(){
                 Linux 是一種開放和免費的源代碼，可以替代window 作系統用。另外，Linux 的操作系統提供了不同工具和支持多種語言令開發者更自由選擇。
             </p>
 
-        </div>
+        </Container>
         
-        <div class="content" id="content-ls">
+        <Container >
             <ConTitle order={2}>
                 ls
             </ConTitle>
@@ -43,8 +44,8 @@ function ConContent(){
 
 
             <Image src="/img/img1.png" id="img1" width='500' height="300" alt = " linux"/ >
-        </div>
-        <div class="content" id="content-cd">
+        </Container>
+        <Container class="content" id="content-cd">
             <ConTitle order={3}>
                 cd
             </ConTitle>
@@ -56,9 +57,9 @@ function ConContent(){
                 示範
             </ConTitle>
             <Image src="/img/img2.png" id="img2" width="500" height='100' alt = " linux"/>
-        </div>
+        </Container>
 
-        <div class="content" id="content-cat">
+        <Container >
             <ConTitle>
                 cat
             </ConTitle>
@@ -72,8 +73,8 @@ function ConContent(){
             <code>{`cat filename.txt]     顯示檔案內容  `}</code>
             <code>{` cat ['文字或檔案名稱(file2name.txr)] {'>'} ['filename.txt']   快速創建檔案並將內容寫入檔案 `}</code>
             <code>{`cat *.['副檔名']    例如 *.txt   顯示所有txt的檔案內容 `} </code>          
-        </div>
-        </div>
+        </Container>
+        </Container>
     </>
     );
 }
