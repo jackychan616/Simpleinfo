7a32174acc40bca1db1ecf7d0c4655f355929e03
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-03-09 20:35:58 +0800

    fix

--- src/pages/content/ai-tutorial/index.js
+++ src/pages/content/ai-tutorial/index.js
@@ -5,7 +5,7 @@ import { ArtiCard } from '../../components/card';
 import Meta from '../../components/meta';
 import { Container } from '@mantine/core';
 const BlogList=[
-    {name:'如何使用AI繒圖？',path:'ai-tutorial/photo-ai-tutorial',img:'/img/ai-generate-img.jpg',date:'1/3/2023'},
+    {name:'如何使用AI繒圖？',path:'ai-tutorial/photo-ai-tutorial',img:'/img/ai-generate-img.webp',date:'1/3/2023'},
     {name:'如何在Windows本地部署Stable Diffusion?',path:'ai-tutorial/setup-stable-diffusion',img:'/img/stable-diffusion.webp',date:'1/3/2023'}
 ]
 
@@ -14,7 +14,11 @@ export default function content(){
     return (
         <>
             <Container>
-                <Page hTitle="AI 教學" img="/img/ai.jpg">
+                <Page hTitle="AI 教學" img="/img/ai.jpg"
+                 fill
+                 sizes="(max-width: 768px) 100vw,
+                         (max-width: 1200px) 50vw,
+                         33vw">
                 <ArtiCard data={BlogList}/>
                 </Page>
             </Container>
