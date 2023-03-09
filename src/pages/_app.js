dfdeae1ec959e03d9a95bf86253a8e9217645a14
Jacky Chan <82193805+jackychan616@users.noreply.github.com>
2023-03-09 03:18:46 +0000

    vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css'; 
 import Layout  from './components/layout';
+import { Analytics } from '@vercel/analytics/react';
 
 
 export default function MyApp({ Component ,pageProps}) {
   return  (
     <Layout>
       <Component {...pageProps} />
+      <Analytics/>
     </Layout>
     
   );
