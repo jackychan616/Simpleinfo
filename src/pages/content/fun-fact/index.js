27b2a949b981ae46f0108dd8ff309fcfc637c4eb
jackychan616 <82193805+jackychan616@users.noreply.github.com>
2023-03-10 16:32:08 +0800

    no fix

--- src/pages/content/fun-fact/index.js
+++ src/pages/content/fun-fact/index.js
@@ -3,7 +3,7 @@ import { Container } from '@mantine/core';
 import { ArtiCard } from '../../components/card';
 import Meta from '../../components/meta';
 import Page from '../../components/page_index';
-const BlogData=[
+const Bloglist=[
     {name:'中國古代最重要的君主(上)',path:'/fun-fact/chinese-king1',img:'/img/chinese-king1.jpg',date:'3/3/2023'},
     {name:'中國古代最重要的君主(下)',path:'/fun-fact/chinese-king2',img:'/img/chinese-king2.jpeg',date:'3/3/2023'},
     {name:'古人的性癖好 孫中山是蘿莉控?',path:'/fun-fact/Sun-Yat-sen',img:'/img/Sun-Yat-Sen.jpeg',date:'6/3/2023'}
@@ -14,7 +14,7 @@ export default function ConPage (){
     return(
         <>
             <Page hTitle={"有趣小知識"} img="/img/fact.webp">
-                <ArtiCard data={BlogData}/>
+                <ArtiCard data={Bloglist}/>
             </Page>
             <Meta description="有趣小知識,中國近代史,中國歷史"  img = "https://simpleinfo.live/img/Sun-Yat-Sen.jpeg"/>
             
--- src/pages/content/sport/index.js
+++ src/pages/content/sport/index.js
@@ -4,7 +4,7 @@ import { Container } from "@mantine/core";
 import {ArtiCard} from "../../components/card";
 import Meta from "../../components/meta";
 import { Page } from '../../components/page_index';
-const BlogList=[
+const Bloglist=[
     {name:"海布里之王-亨利",path:"/sport/football-henry",date:'',img:'/img/football-henry.jpg'}
 ]
 
@@ -14,7 +14,7 @@ export default function ConPage(){
             <Meta subtitle={"體育"} description="體育"/>
             <Container>
                <Page title="體育" img="https://library.sportingnews.com/styles/facebook_1200x630/s3/2021-08/world-cup-trophy-stand_133kk6nwawbmj16uhxvaiznup2.jpg?itok=bYPSCbur" hTitle="體育">
-                    <ArtiCard data={BlogList}/> 
+                    <ArtiCard data={Bloglist}/> 
                 </Page>
             </Container>
             
