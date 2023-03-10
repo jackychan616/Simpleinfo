4e2a7411b3e7ba70a43361e5fb56017d0a853ad3
Jacky Chan <82193805+jackychan616@users.noreply.github.com>
2023-03-10 05:17:11 +0000

    Fix meat image

--- src/pages/main-page.js
+++ src/pages/main-page.js
@@ -123,7 +123,7 @@ export default function App(){
     return(
         <>
             <Meta keywords={"博客,中文,資訊,資訊平臺,香港,香港博客,Hong Kong Blog,Blog,教學,教學類型,教學博客,教學 Blog,AI教學,AI,有趣資訊 "}
-              description={"香港資訊類型博客,專注於提供最新的電腦,遊戲,AI等資訊"} img = {"img/simple_info.png"}
+              description={"香港資訊類型博客,專注於提供最新的電腦,遊戲,AI等資訊"} img = {"https://simpleinfo.live/img/simple_info.png"}
               />
 
             <Body />
