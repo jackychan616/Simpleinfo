19a39fe854d0f00e9281142e43e6bfe75d6ad49f
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-03-11 09:25:51 +0800

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import loadable from '@loadable/component';
 import { Children } from 'react';
 import Head from 'next/head';
-import pMinDelay from 'p-min-delay';
-import Loading from './loading';
-const App =loadable(() => pMinDelay(import('./main-page'),500) );
+import App from './main-page';
+
 export default function Home() {
   return (
-    <App fallback={<Loading/>}/>
+    <App/>
   )
 }
