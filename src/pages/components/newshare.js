55feb034662f3587f653641914f9b6a4a822e7f5
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-03-11 15:06:06 +0800

    share box

--- src/pages/components/newshare.js
+++ src/pages/components/newshare.js
@@ -1,40 +1,14 @@
-import { Menu, Button, Text, Image } from '@mantine/core';
-import Tooltip from '@mui/material/Tooltip';
-import IconButton from '@mui/material/IconButton';
-import ShareIcon from '@mui/icons-material/Share';
-import dynamic from "next/dynamic"
-
-
-export function Sharebutton({children,url}) {
+import {Group} from '@mantine/core';
+import {FiShare2} from 'react-icons/fi';
+import {AiFillSave,AiOutlineLike,AiOutlineDislike} from 'react-icons/ai';
+export default function ButtonBox({children,url}) {
     return(
-        <>
-        <Menu shadow="md" width={200}>
-            <Menu.Target>
-                <Tooltip title="Share" >
-                    <IconButton >
-                        <ShareIcon color='primary'/ >
-                    </IconButton>
-                </Tooltip>
-            </Menu.Target>
-            <Menu.Dropdown>
-                <Menu.Item component='a' onClick={() => {navigator.clipboard.writeText(url)}}>
-                    複製鏈結
-                </Menu.Item>
-                <Menu.Item scr = "/img/facebook.png">
-                    Share to facebook
-                </Menu.Item>
-            </Menu.Dropdown>
-        </Menu>
-        {children}
-        </>
+    <Group>
+        <FiShare2/>
+        <AiFillSave/>
+        <AiOutlineLike/>
+        <AiOutlineDislike/>
+    </Group>
         
     );
 }
-
-function Context(){
-  return (
-    <>
-    </>
-  );
-}
-export default Context;
