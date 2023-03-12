b70cd43435b400cfa5976d674028d294e2d524e1
jackychan616 <82193805+jackychan616@users.noreply.github.com>
2023-03-12 15:27:38 +0800

    fix

---
+++ src/pages/content/news/Faye-Wong-song.js
@@ -0,0 +1,23 @@
+import {ConTitle,ConText} from '../../components/component';
+import {Container} from '@mantine/core';
+import Image from 'next/image';
+import { Sharebutton } from 'src/pages/components/share';
+import Meta from 'src/pages/components/meta';
+import { AppsOutage } from '@mui/icons-material';
+
+function Body(){
+    return (
+        <Container>
+            <ConTitle>
+
+            </ConTitle>
+        </Container>
+    )
+}
+export default function page(){
+    return (
+        <>
+            <Body/>
+        </>
+    )
+}
