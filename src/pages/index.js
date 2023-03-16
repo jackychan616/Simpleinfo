cc9ab83077b780efdafe9982f0c88f6a53e7e7a5
猴貓 <a102009102009@gmail.com>
2023-03-16 21:20:06 +0800

    fix

--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import App from './main-page';
-
-export default function Home() {
-  return <App />;
-}
--- src/pages/loading.js
+++
@@ -1,9 +0,0 @@
-import { Loader, Container } from '@mantine/core';
-
-export default function Loading() {
-  return (
-    <Container fluid={true}>
-      <Loader color="indigo" size="xl" variant="bars" />
-    </Container>
-  );
-}
