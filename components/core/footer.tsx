17f1f5cc25b47c74d93ed57015807a2d0cf31ddb
Jacky Chan <82193805+jackychan616@users.noreply.github.com>
2023-03-17 07:11:46 +0000

    basic colour

--- components/core/footer.tsx
+++ components/core/footer.tsx
@@ -1,3 +1,12 @@
-export default function Meta() {
-  return <footer></footer>;
+export default function Footer() {
+  return <footer>
+    <div style = {{
+      backgroundColor : "#00DEFF",
+     height : "100px",
+     width: "100px",
+     position: "fixed",
+     bottom : "10px"}}>
+      <text>Simpleinfo</text>
+    </div>
+  </footer>;
 }
