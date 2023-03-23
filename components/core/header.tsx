1273ac817c363c54392e5780d57055567f57afb5
Jason Chan <80382634+jasonchanjj123@users.noreply.github.com>
2023-03-23 07:26:49 +0000

    50%

--- components/core/header.tsx
+++ components/core/header.tsx
@@ -1,23 +1,34 @@
-import { Navbar, Text ,useTheme,Button } from '@nextui-org/react';
+import { Navbar, Text ,useTheme,Button ,changeTheme } from '@nextui-org/react';
 import React from "react";
 import {IconMoon,IconSunHigh} from '@tabler/icons-react';
 export default function Header() {
-  const isDark = useTheme();
-  const [activeColor, setActiveColor] = React.useState("primary");
-  return (
-    <Navbar isBordered={isDark} variant="sticky">
+  const {type, isDark} = useTheme();
+  const handleChange = () => {
+    const nextTheme = isDark ? 'light' : 'dark';
+    window.localStorage.setItem('data-theme', nextTheme); // you can use any storage
+    changeTheme(nextTheme);
+  };
+  const DarkModeToggle =()=>(isDark ? <IconMoon /> :<IconSunHigh />);
+
+  return(
+    <Navbar isBordered variant="sticky">
       <Navbar.Brand>
         <Text>Simple Info</Text>
       </Navbar.Brand>
-      <Navbar.Content activeColor={activeColor} variant="underline">
+      <Navbar.Content activeColor='primary' variant="underline">
         <Navbar.Link href="/">首頁</Navbar.Link>
         <Navbar.Link href="/">文章</Navbar.Link>
       </Navbar.Content>
       <Navbar.Content>
-        <Button 
-        color={isDark? 'yel     low':'primary'}
-                icon={isDark ?<IconMoon/> :<IconSunHigh/>}
-        />
+        <Navbar.Item>
+          <Button 
+            size="xs"
+            ghost
+            onClick={handleChange}
+            icon={<DarkModeToggle/>}
+        />  
+        </Navbar.Item>
+        
       </Navbar.Content>
     </Navbar>
   );
