2a5c85e9cbb8615decd3479ab8d49f182b31cc8e
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-03-23 16:49:01 +0800

    fix

--- src/pages/content/ai-tutorial/ai-futher-develop.js
+++ src/pages/content/ai-tutorial/ai-futher-develop.js
@@ -1,13 +1,55 @@
 import {Container} from '@mantine/core';
-import {ConText,ConTitle} from '../../components/component';
+import {ConText,ConTitle,List} from '../../components/component';
 
 export default function Page(){
     return(
         <Container>
             <ConTitle order={1} size="h1">AI未來發展的前景,商業分析竟佔重要地位</ConTitle>
             <ConText>隨著科技快速發展，人工智慧 (AI) 已經成為一個越來越受到關注的技術領域。AI的應用可以幫助企業提高效率、改善產品品質、為人類解決許多問題。未來，AI的發展前景非常廣闊， 亦能接手很多人類的工作。</ConText>
-            <ConTitle order={2} size="h2">自動駕駛汽車</ConTitle>
-            <ConText></ConText>
+            <ConTitle order={2} size="h2">一丶自動駕駛汽車</ConTitle>
+            <ConText>自動駕駛汽車是AI發展的一個重要領域，過去幾年，越來越多的公司開始投入到自動駕駛技術的研究和開發中，目前已經有一些自動駕駛汽車開始在美國和國外的道路上進行測試。許多車廠已經開始將AI技術應用在汽車產品的生產中，未來自動駕駛汽車將大大提高行車的安全性、節省人們的時間和人力成本。</ConText>
+            <ConText> 另外，自動駕駛汽車的關鍵技術包括傳感器、算法、智能控制系統等，公司需要進行多項技術研究和實驗來不斷提升性能和安全性。自動駕駛汽車的商業模式也需要不斷探索和創新，各家公司嘗試將自動駕駛技術應用於出租車、貨運、物流等領域。</ConText>
+            <ConText>自動駕駛汽車的技術已經很成熟了，很多公司已經發布了自動駕駛汽車的商業化計劃，其中包括 Waymo、Tesla、Uber、騰訊...等</ConText>
+            <ConTitle order={2} size="h2">二丶醫療保健</ConTitle>
+            <ConText>人工智能可以在醫療照護和保健領域內發揮作用。這包括智慧醫療決策、協助患者定制個性化的治療方案、預測疾病風險、為藥物開發提供更有效的方法等。</ConText>
+            <ConTitle order={3} size="h3">智慧醫療決策</ConTitle>
+            <ConText>智慧醫療決策是指基於人工智能和大數據技術的醫療決策支持系統，可以提高醫生的診斷和治療決策的準確性和效率。智慧醫療決策的發展可以幫助醫護人員更好地應對各種慢性疾病和急診情況，尤其是在醫療資源相對缺乏的地區。</ConText>
+            <ConText>智慧醫療決策的主要特點:</ConText>
+            <List type="ordered">
+                <List.Item>大數據：智慧醫療決策系統需要大量的病歷數據、影像數據、檢測數據等，以此為基礎，進行數據分析和挖掘。</List.Item>
+                <List.Item>機器學習：智慧醫療決策系統需要結合機器學習技術，從大數據中學習病例的特徵和模式，並進行預測和建模。</List.Item>
+                <List.Item>推薦系統：智慧醫療決策系統需要基於數據挖掘的結果，為醫生提供相關的治療方案和建議，從而幫助其做出更準確的診斷和治療決策。</List.Item>
+            </List>
+            <ConText>智慧醫療決策系統可以幫助醫護人員提高工作效率和應對各種複雜病例的能力，同時還可以減少醫療失誤和慢性疾病的治療負擔，提高醫療質量和效率。</ConText>
+            <ConTitle order={3} size="h3">預測疾病風險</ConTitle>
+            <ConText>隨著人工智能技術和大數據的不斷進步，AI能夠快速而準確地分析和預測疾病的風險，從而幫助醫生更好地制定治療計劃以及預防措施，提高患者的護理質量。</ConText>
+            <ConTitle order={4} size="h4">應用</ConTitle>
+            <List>
+                <List.Item>基於患者數據的風險評估：AI可以檢測由患者的年齡，性別，體重，身高，血壓等數據產生的風險，從而提供個性化的治療計劃。</List.Item>
+                <List.Item>藥物預測：AI可以分析所有的病例、患者和療法數據，從中找到和某種疾病的最佳治療方案。</List.Item>
+                <List.Item>病因研究：由於AI可以分析數據量非常大的數據集並挖掘關聯性，因此它能夠幫助研究人員加深對疾病形成的原因及其風險因素的認識，利於制定更優秀的治療策略。</List.Item>
+            </List>
+            <ConText>總結來說，AI醫療保健在預測疾病風險上的應用前景非常廣闊。通過分析大量的數據，AI可以提高治療的成功率，縮短治療的時間，使患者的經歷更加愉快和舒適。但需要有足夠的數據支持，同时要确保 AI 系统和数据的安全性和可靠性，以便在實節中取得良好的結果。</ConText>
+            <ConTitle order={2} size="h2">三丶商業分析</ConTitle>
+            <ConText>AI商業分析（Artificial Intelligence Business Analytics），是一種利用人工智慧技術和大數據分析技術來提高企業決策能力和日常業務活動效率的分析方法。它結合了機器學習、自然語言處理、圖像識別、深度學習和統計學等多種技術，可以從海量的數據中提取出有意義的信息，幫助企業獲得更深入的商業理解並做出更好的決策。</ConText>
+            <ConTitle order={3} size="h3">重要應用場景</ConTitle> 
+            <List type="ordered">
+                <List.Item>數據挖掘和分析：主要包括數據清洗、數據挖掘、數據可視化等技術。通過這些技術，企業可以從海量的數據中提取出有用的信息，並進行剖析和分析，以幫助決策者解決商業問題。</List.Item>
+                <List.Item>預測分析：透過機器學習、深度學習等技術，AI商業分析可以利用歷史資料和疊加一系列影響因素，對未來的趨勢和結果進行預測，從而協助企業做出戰略抉擇和決策。</List.Item>
+                <List.Item>優化分析：利用AI算法對企業的運作流程進行分析優化，從而最小化成本、擴大營收、提高產品質量等，幫助企業獲得更好的商業成績。</List.Item>
+                <List.Item>社交媒體分析：通過分析社交媒體的大量數據，AI商業分析可以更好地了解消費者需求和行為，從而幫助企業了解市場趨勢，制定更精準的行銷策略。</List.Item>
+            </List>
+            <ConTitle order={3} size="h3"> AI帶給商業分析的好處</ConTitle>
+            <List type="ordered">
+                <List.Item>降低成本：AI商業分析可以快速分析和預測，降低企業成本。</List.Item>
+                <List.Item>精確預測：相比傳統的統計學方法，AI商業分析可以更加準確地進行預測和分析。</List.Item>
+                <List.Item>實時決策：AI商業分析可以快速分析數據，幫助企業快速做出決策。</List.Item>
+                <List.Item>企業效率提升：AI商業分析可以提高企業效率，節省時間和成本，幫助企業更好地把握市場趨勢。</List.Item>
+            </List>
+            <ConText>隨著技術和算法不斷成熟，AI商業分析能夠協助企業迅速地應對商業挑戰，提高競爭力。然而，企業在使用AI商業分析時必須注意保障數據安全和保密性，并確保數據的真實性和準確性，以避免任何潛在的風險。</ConText>
+            <ConTitle order={2} size="h2">四丶智慧城市</ConTitle>
+            <ConText>智慧城市是指利用新興科技體系，裝置智慧感測器、自動化作業流程，來改善居民生活品質、增進城市運轉效率的城市模式。這些新興科技體系可以包括物聯網、大數據、人工智慧、區塊鏈等等。</ConText>
+            <ConTitle order={}></ConTitle>
         </Container>
     );
 }
