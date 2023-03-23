183c47967143e4994ded4ce13ae8aaa6a5810478
Jason Chan <80382634+jasonchanjj123@users.noreply.github.com>
2023-03-23 06:56:01 +0000

    30%

--- components/core/header.tsx
+++ components/core/header.tsx
@@ -1,14 +1,23 @@
-import { Navbar, Text } from '@nextui-org/react';
-
+import { Navbar, Text ,useTheme,Button } from '@nextui-org/react';
+import React from "react";
+import {IconMoon,IconSunHigh} from '@tabler/icons-react';
 export default function Header() {
+  const isDark = useTheme();
+  const [activeColor, setActiveColor] = React.useState("primary");
   return (
-    <Navbar variant="sticky">
+    <Navbar isBordered={isDark} variant="sticky">
       <Navbar.Brand>
         <Text>Simple Info</Text>
       </Navbar.Brand>
-      <Navbar.Content variant="underline">
+      <Navbar.Content activeColor={activeColor} variant="underline">
         <Navbar.Link href="/">首頁</Navbar.Link>
         <Navbar.Link href="/">文章</Navbar.Link>
+      </Navbar.Content>
+      <Navbar.Content>
+        <Button 
+        color={isDark? 'yel     low':'primary'}
+                icon={isDark ?<IconMoon/> :<IconSunHigh/>}
+        />
       </Navbar.Content>
     </Navbar>
   );
