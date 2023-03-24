b2bddb5632a66f4bd42c53128f9f32b9ef5a147b
jackychan616 <82193805+jackychan616@users.noreply.github.com>
2023-03-24 21:53:15 +0800

    50% fixed loader

--- src/pages/loading.js
+++ src/pages/loading.js
@@ -1,9 +1,17 @@
 import { Loader, Container } from '@mantine/core';
+import styles from './page.module.css';
+import { ReactElement } from "react";
 
-export default function Loading() {
+
+function Loading() {
   return (
-    <Container fluid={true}>
-      <Loader color="indigo" size="xl" variant="bars" />
-    </Container>
+      <div className={styles.loader_div}>
+          <Loader color="indigo" size="xl" variant="bars" />
+      </div>
   );
 }
+Loading.getLayout = function getLayout(page) {
+  return <>{page}</>;
+};
+
+export default Loading;
