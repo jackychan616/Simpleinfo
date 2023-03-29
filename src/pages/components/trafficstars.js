63262bb7d59f047cd2d6f3805b1310b6c51649b7
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-03-29 21:09:46 +0800

    added desktop ads

--- src/pages/components/trafficstars.js
+++ src/pages/components/trafficstars.js
@@ -1,5 +1,9 @@
 export default function TrafficStarsAds(){
     return(
+        <>
         <iframe width="300" height="250" frameborder="0" scrolling="no" src="//tsyndicate.com/iframes2/65a342bbef6449e6be625fb8440cbbe7.html?extid={extid}"></iframe>
+        <iframe width="300" height="250" frameborder="0" scrolling="no" src="//tsyndicate.com/iframes2/65a342bbef6449e6be625fb8440cbbe7.html?extid={extid}"></iframe>
+        </>
+        
     );
 }
