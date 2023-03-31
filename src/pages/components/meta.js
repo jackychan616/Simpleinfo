759eaa01793db68b2e3f4cce190b7c1c6bda2ae4
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-03-31 22:09:54 +0800

    fix

--- src/pages/components/meta.js
+++ src/pages/components/meta.js
@@ -2,7 +2,7 @@ const siteTitle = "Simple Info HK";
 const defaultDescription ="香港資訊類型博客,專注於提供最新的電腦,遊戲,AI等資訊";
 import { NextSeo } from 'next-seo';
 
-export default function NextMeta({ pageTitle, keywords, description ,subtitle,img,alt}){
+export default function Meta({ pageTitle, keywords, description ,subtitle,img,alt}){
   const D=description ? description : defaultDescription;
   return(
       <NextSeo
