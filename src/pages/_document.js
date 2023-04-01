c2856a2789f09acc0140f89418dac40bc11ddf15
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-04-01 17:51:22 +0800

    fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,12 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import GoogleAds from './components/googleAds';
 import TrafficStarsAds from './components/trafficstars';
+import Meta from './components/meta';
 export default function Document() {
   return (
     <Html lang="zh-Hant-HK">
       <Head>
         <link rel="icon" type="image/png" href="/icon.png" />
         <meta charSet="utf-8" />
+        <Meta/>
       </Head>
       <body>
         <Main />
