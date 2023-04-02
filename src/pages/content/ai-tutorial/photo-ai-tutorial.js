cd4bf88b3445cc3a07e6618d6f1184dd8cda64a5
jackychan616 <82193805+jackychan616@users.noreply.github.com>
2023-04-02 12:59:57 +0800

    fix

--- src/pages/content/ai-tutorial/photo-ai-tutorial.js
+++ src/pages/content/ai-tutorial/photo-ai-tutorial.js
@@ -1,10 +1,8 @@
 import { useState } from 'react';
 import styles from '../../page.module.css'
-import Footers from '../../components/footer'
 export default function Page() {
   return (
       <>
-        <Footers/>
       </>
   );
 } 
