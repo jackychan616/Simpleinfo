e40675f247f07dcb4ce63e0206b7b3ad434c4d67
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-04-10 23:08:16 +0800

    fix

--- src/pages/content/ai-tutorial/openxla-quick-tutorial.js
+++ src/pages/content/ai-tutorial/openxla-quick-tutorial.js
@@ -5,7 +5,7 @@ import {Meta} from '../../components/meta';
 function Page(){
     return(
         <Container>
-            <ConTitle order={1} size="h1">OpenXLA:史上最強開源項目，Google、微軟、蘋果、Meta，整合現有框架和硬件</ConTitle>
+            <ConTitle order={1} size="h1">OpenXLA:史上最強開源項目，Google、微軟、蘋果、Meta，整合現有AI框架和硬件</ConTitle>
 
             <ConText>2022年 10 月的 Google Cloud Next 2022 活動中，OpenXLA 項目正式浮出水面，Google與包括Meta、Apple、AMD、Arm、亞馬遜、英特爾、英偉達、阿里巴巴等科技公司推動的開源 AI 框架合作，致力於匯集不同機器學習框架，讓機器學習開發人員獲得能主動選擇框架、硬件的能力。</ConText>
             <Image alt="OpenXLA" maw={650} caption="OpenXLA" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgBvB_qhPf0xhQnOcL4hIPp0HVPh5TvRDNUnMbwlYJx7_u0SBcHGjE_NiT0MgUsYzJmIAX5i5Z7ZoAVYdzuOuQmw9CY63a3rwPcDvv44l-zD9n33KUS2MlfteVGEfiNtM0dw_KI-AS8jl0AfIiSRKfUtB9k2DDgx6Jw_KkMF5AMwjxqKuVQLQyelMMr/s1600/OpenXLA-Logo-RGB-HORIZ-LIGHT-BG.png"/>
