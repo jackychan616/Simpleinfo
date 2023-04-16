6f2e4998e8145f12c1c7c53f192a8c82d6c9ad7e
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-04-16 14:40:28 +0800

    fix

--- src/pages/content/ai-tutorial/index.js
+++ src/pages/content/ai-tutorial/index.js
@@ -70,10 +70,7 @@ export default function content() {
         <Page
           hTitle="AI 教學"
           img="/img/ai.webp"
-          fill
-          sizes="(max-width: 768px) 100vw,
-                (max-width: 1200px) 50vw,
-                33vw"
+          maw={500}
         >
           <ArtiCard data={Bloglist} />
         </Page>
