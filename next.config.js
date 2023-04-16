3713f0631af2f00275f137e332cdd588442e6290
jackychan616 <82193805+jackychan616@users.noreply.github.com>
2023-04-16 10:45:26 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -5,6 +5,12 @@ const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md'],
   images: { unoptimized: true },
+  webpack5: true,
+  webpack: (config) => {
+      config.resolve.fallback = { fs: false };
+  
+      return config;
+    },
 };
 
 module.exports = nextConfig;
