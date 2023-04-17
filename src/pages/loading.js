31eac53b80311832181652afb94131e930689c75
jackychan616 <82193805+jackychan616@users.noreply.github.com>
2023-04-17 16:44:00 +0800

    fix

--- src/pages/loading.js
+++ src/pages/loading.js
@@ -1,19 +1,22 @@
 import { Loader, Container, Space, Center } from '@mantine/core';
 import styles from './page.module.css';
 import { ReactElement } from "react";
+import { blue } from '@mui/material/colors';
 
 
 function Loading() {
   return (
-      <div className={styles.div_simple}>
+      <div className={styles.div_loader}>
             <Loader color="indigo" size="xl" variant="bars" />
             <span><Space h="xm" /></span>
+          <div className={styles.div_simple_word}>
             <span><h className = {styles.s}>S</h></span>
             <span><h className={styles.i}>i</h></span>
             <span><h className= {styles.m}>m</h></span>
             <span><h className = {styles.p}>p</h></span>
             <span><h className = {styles.l}>l</h></span>
             <span><h className = {styles.e}>e</h></span>
+          </div>
       </div>
   );
 }
