368d3ffac712ccf1cfd4cd273efbd6ff87c5c70d
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-04-17 17:37:11 +0800

    fix

--- src/pages/components/page_index.js
+++ src/pages/components/page_index.js
@@ -1,11 +1,11 @@
-import {Title,Container,Image} from '@mantine/core';
+import {Title,Container,Image,Center} from '@mantine/core';
 
 
 export function Page({children,title,hTitle,img}){
     return (
         <>
             <Container >
-                <Title order={1}>{hTitle}</Title>
+                <Center><Title order={1}>{hTitle}</Title></Center>
                 <div>
                     <Image maw={700} src={img} alt=""/>   
                 </div>
