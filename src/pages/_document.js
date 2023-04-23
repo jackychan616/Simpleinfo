04f9c3a89229075de1014e6a5c1f01bec702504b
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-04-23 21:16:47 +0800

    fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import {GoogleAds} from './components/googleAds';
 export default function Document() {
   return (
     <Html lang="zh-Hant-HK">
       <Head/>
       <body>
+        <GoogleAds/>
         <Main />
         <NextScript />
       </body>
