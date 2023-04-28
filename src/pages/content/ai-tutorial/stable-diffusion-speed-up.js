de984b2a9328c9823bc7fb00dba299e9d53b4648
Jason Chan <80382634+jasonchanjj123@users.noreply.github.com>
2023-04-28 10:54:51 +0800

    Update stable-diffusion-speed-up.js

--- src/pages/content/ai-tutorial/stable-diffusion-speed-up.js
+++ src/pages/content/ai-tutorial/stable-diffusion-speed-up.js
@@ -1,1 +1,15 @@
 
+import {Container,List} from '@mantine/core';
+import {ConText,ConTitle} from '../../components/component';
+
+export default function Page(){
+    return(
+    <Container>
+     <ConTitle>
+      Google發表論文 實現在Samsung S23 ultra用stable difffusion 生成圖片 僅需11秒
+      </ConTitle>
+    </Container>
+    );
+
+}
+
