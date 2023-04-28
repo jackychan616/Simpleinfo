9e8bedeb3fe94052584b3669fdfee0ccaed66536
Jason Chan <80382634+jasonchanjj123@users.noreply.github.com>
2023-04-28 11:55:52 +0800

    Update assemblyaI.js

--- src/pages/content/ai-tutorial/assemblyaI.js
+++ src/pages/content/ai-tutorial/assemblyaI.js
@@ -5,7 +5,7 @@ function Page(){
     return(
     <>
         <ConTitle>AssemeblyAI 強大的AI語音識別工具，快速整合影片重點、內容摘要、主題</ConTitle>
-        <ConText>AssemblyAI是强大的工具，它來幫助你轉錄和理解音頻。它能夠預先錄制的音頻和視頻文件以及實時音頻流自動轉換為文本轉錄。</ConText>\
+        <ConText>AssemblyAI是强大的工具，它來幫助你轉錄和理解音頻。它能夠預先錄制的音頻和視頻文件以及實時音頻流自動轉換為文本轉錄。</ConText>
         <ConTitle order={2} size="h2">功能</ConTitle>
         <ConText></ConText>
     </>);
