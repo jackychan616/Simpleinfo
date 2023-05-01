85e74f61bc6e1a4093903221c6d77b7520222b13
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-05-01 13:54:22 +0800

    fix

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+// Default code you can customize according to your requirements.
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://simpleinfohk.me',
+    generateRobotsTxt: true,   
+}
