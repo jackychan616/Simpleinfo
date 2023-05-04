8a21331c425360d618c4f25d7b820e89fd7c60a0
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-05-04 21:39:38 +0800

    fix

--- src/pages/content/sport/spain-new-football-player.js
+++ src/pages/content/sport/spain-new-football-player.js
@@ -10,14 +10,14 @@ export default function Page(){
                 <title>未來西班牙女王的駙馬？西班牙足球新秀加維</title>
             </Head>
             <Container>
-                <ConTitle order={1} maw={750} size="h2">未來西班牙女王的駙馬？西班牙足球新秀加維</ConTitle>
+                <ConTitle order={1} maw={550} size="h2">未來西班牙女王的駙馬？西班牙足球新秀加維</ConTitle>
                 <ConText>加維，自從在2021年加入巴塞隆拿主隊後，在埸上不論運球、預判、智慧、視野、傳球都有著不凡的表現。他憑着實力贏得許多巴塞隆拿和西班牙球迷的心，因為他跳入了高級足球，尤其是西班牙王室的球迷。</ConText>
                 <Space h="lg"/>
                 <Image src="/img/spain-football-new-player/gavi.jpg" alt="加維" caption=""/>
                 <Space h="lg"/>
                 <ConText>根據不少當地傳媒報道，西班牙王儲妃萊昂諾爾對這名中場球員有些迷戀。</ConText>
                 <Space h="lg"/>
-                <Image src="/img/spain-football-new-player/spain-princess.jpg" maw={600}alt="西班牙公主 "/>
+                <Image src="/img/spain-football-new-player/spain-princess.jpg" maw={400}alt="西班牙公主 "/>
                 <Space h="lg"/>
                 <ConText>根據他們的信息，她的學校文件夾裡全是加維的照片。此外，據報導，有人看到國王費利佩六世去迎接加維，並為他的女兒遞給他一件有加維簽名襯衫。</ConText>
                 <ConText>《馬卡報》援引知情人士的說話，表示西班牙王室對這種關係沒有任何問題。《馬卡報》還報導說，加維的隊友已經開始稱他為“小王子”。究竟他們能否修成正果呢？這就有待之後的發展了。</ConText>
