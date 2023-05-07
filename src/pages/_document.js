e988e4f07fc5319e2156a4158ae7225c249d643e
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-05-07 20:32:47 +0800

    fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,19 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 export default function Document() {
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-L6NG9DRBHS');
   return (
     <Html lang="zh-Hant-HK">
       <Head>
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3459129696587268"
      crossOrigin="anonymous"
      strategy="lazy"></script>
-     <Script async src="https://www.googletagmanager.com/gtag/js?id=G-L6NG9DRBHS"></Script>
-    
       </Head>
       <body>
         <Main />
