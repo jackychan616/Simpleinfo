c8d7d977b56c6de31577eca239486e6f29ee3166
Jason Chan <80382634+jasonchanjj123@users.noreply.github.com>
2023-05-08 16:25:56 +0800

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3459129696587268"
      crossOrigin="anonymous"
      strategy="lazy"></script>
+     <script src="//cdn.tsyndicate.com/sdk/v1/bi.js" data-ts-spot="6492ddbe38d2421387bc5eecaabe6c05" data-ts-width="300" data-ts-height="250" data-ts-extid="{extid}" async defer></script>
       </Head>
       <body>
         <Main />
